1894247ea51bc10d9d8b1ebec97f2dcef8702d3a
dillen <joseph_dillner@hotmail.com>
2016-04-14 13:20:54 +0200

    provat igen

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import com.dillner.Student;
 
 public class Main {
  
 	public static void main(String[] args) {
 		System.out.println("hello");
+		
+		Student student = new Student("Lars");
+		
+		System.out.println("Name of student: " + student.getName());
 	} 
 }
